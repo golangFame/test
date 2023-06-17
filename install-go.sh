5363888d6dd2c288e58da00e851f8a992fb27ab2
Hiro <laciferin@gmail.com>
2023-06-17 01:48:34 +0000

    update install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -1,2 +1,6 @@
-go install golang.org/dl/go1.14@latest
-go1.14 download
+#!/bin/sh
+
+go install golang.org/dl/go1.14@latest & go1.14 download ;
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer);
+zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer);
+source /home/codespace/.gvm/scripts/gvm
